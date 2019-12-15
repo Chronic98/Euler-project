314b3d9178e7a2a932e5778183062248b4fcc729
Женя Данчук <45733750+Chronic98@users.noreply.github.com>
2019-12-15 23:28:27 +0200

    Сделал 6

---
+++ 6.py
@@ -0,0 +1,16 @@
+def sum_numder_v_kv():
+    number = 0
+    for x in range(1, 101):
+        number += x
+    return number * number
+
+
+def sum_kvadratov():
+    number = 0
+    for x in range(1, 101):
+        number += x * x
+    return number
+
+
+razniza = sum_numder_v_kv() - sum_kvadratov()
+print(razniza)
