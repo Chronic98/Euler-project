07566d7b89f6f4acd3993a9fea91dd67b426554e
Женя Данчук <45733750+Chronic98@users.noreply.github.com>
2019-12-15 22:48:59 +0200

    Сделал 4

---
+++ 4.py
@@ -0,0 +1,11 @@
+number = 0
+palidrom = 0
+for i in range(100, 1000):
+    for x in range(100, 1000):
+        number = i * x
+        n = str(number)
+        n_palidrom = n[::-1]
+        if number == int(n_palidrom):
+            if number > palidrom:
+                palidrom = number
+print(palidrom)
