b7f4bed593e1a340e2e0bcaa91b32049c892a0ed
Женя Данчук <45733750+Chronic98@users.noreply.github.com>
2019-12-15 23:08:47 +0200

    Сделал 5

---
+++ 5.py
@@ -0,0 +1,14 @@
+stop = 0
+i = 0
+while stop != 1:
+    iter = 0
+    i += 1
+    number = 20 * i
+    for x in range(1, 21):
+        iter += 1
+        if number % x == 0:
+            if iter == 20:
+                print(number)
+                stop = 1
+        else:
+            break
