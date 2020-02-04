520bcd4634956c9825bc1673b3fe5419eaa8486c
Женя Данчук <45733750+Chronic98@users.noreply.github.com>
2020-02-05 00:01:10 +0200

    сделал 9-ю

--- 9.py
+++ 9.py
@@ -9,11 +9,13 @@ import math
 # или
 stop = 0
 list_number_sqrt = []
+# создаю список из целых квадратов чисел до 200000
 for i in range(1, 200000):
     if math.sqrt(i) == int(math.sqrt(i)):
         list_number_sqrt.append(int(math.sqrt(i)))
 
 print(len(list_number_sqrt))
+# так как С самое большое, значит начинать быстрее будет с конца, так же и с В
 for c in list_number_sqrt[::-1]:
     if stop == 1:
         break
